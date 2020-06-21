cfacb66f8bd601c62524a5ab7861216de349c294
yanagswk <yanagisawa3939jp@yahoo.co.jp>
2020-06-21 19:20:46 +0900

    インデント修正

--- src/journal/models.py
+++ src/journal/models.py
@@ -6,7 +6,6 @@ from django.utils import timezone
 class Category(models.Model):
     name = models.CharField(max_length=255)
     slug = models.SlugField(unique=True)
-    timestamp = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
         return self.name
@@ -15,14 +14,12 @@ class Category(models.Model):
 class Tag(models.Model):
     name = models.CharField(max_length=255)
     slug = models.SlugField(unique=True)
-    timestamp = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
         return self.name
 
 
 class Journal(models.Model):
-
     user = models.ForeignKey(CustomUser, verbose_name='ユーザー', on_delete=models.PROTECT)
     title = models.CharField(verbose_name='タイトル', max_length=40)
     category = models.ForeignKey(Category, verbose_name='カテゴリー', on_delete=models.PROTECT)
